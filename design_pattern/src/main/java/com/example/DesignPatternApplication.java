de47128be09d28333e39fe6836efa0a6b1df02f1
wangtao-Allen <wangtao_333@icloud.com>
2018-07-16 11:01:10 +0800

    添加设计模式demo

---
+++ design_pattern/src/main/java/com/example/DesignPatternApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 设计模式
+ */
+@SpringBootApplication
+public class DesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternApplication.class, args);
+	}
+}
---
+++ excel/src/test/java/com/example/service/SaveDataDBServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+// @Suite.SuiteClasses({test.class})
+public class SaveDataDBServiceTest {
+    @Test
+    public void save() throws Exception {
+        System.out.println("hello");
+    }
+}
