ad4e3a296f3d6f137d198033854cb40c5d2351eb
wangtao-Allen <wangtao_333@icloud.com>
2018-07-16 11:09:45 +0800

    冒泡排序 2.0

--- design_pattern/src/main/java/com/example/BubbleSort/BubbleSort.java
+++ design_pattern/src/main/java/com/example/BubbleSort/BubbleSort.java
@@ -7,9 +7,12 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] array = new int[]{5,8,6,3,9,2,1,7};
-        sort(array);
-        System.out.println(Arrays.toString(array));
+        int[] array1 = new int[]{5,8,4,6,3,9,2,1,7};
+        int[] array2 = new int[]{5,8,4,6,3,9,2,1,7};
+        sort1(array1);
+        sort2(array2);
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
     }
 
     /**
@@ -18,7 +21,7 @@ public class BubbleSort {
      * @param array
      * @version 1.0
      */
-    private static void sort(int array[]){
+    private static void sort1(int array[]){
         int tmp = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
@@ -27,6 +30,33 @@ public class BubbleSort {
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
                 }
+            }
+        }
+    }
+
+    /**
+     * 利用isSorted作为标记
+     * 如果本轮排序中有元素交换,则说明数列无序
+     * 如果没有元素交换,则说明数列有序,直接跳出大循环
+     * @param array
+     * @version 2.0
+     */
+    private static void sort2(int array[]){
+        int tmp = 0;
+        for (int i = 0; i < array.length; i++) {
+            //有序标记,每一轮的初始是ture
+            boolean isSorted = true;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j+1]){
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    //有元素交换,所以不是有序,标记变为false
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
             }
         }
     }
